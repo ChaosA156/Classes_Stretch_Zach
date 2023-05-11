10c8ab71665ff9780c0f24a3963d11161ddeef0c
ChaosA156 <114434128+ChaosA156@users.noreply.github.com>
2023-05-11 13:49:56 -0500

    Initial commit

---
+++ Classes_Stretch_Zach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes_Stretch_Zach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Greetings greetings = new Greetings();
+            //
+            greetings.Welcome();
+            //
+
+            //
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+            
+            //
+            greetings.Hello(userName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Classes_Stretch_Zach
+ 
